f56db1d315330f380e93f8d8d3121d49fa770352
Usman Jutt <usmanjutt.kodestudio@gmail.com>
2023-02-02 20:24:52 +0500

    Model and migrations add

--- app/Models/AgentAgreement.php
+++ app/Models/AgentAgreement.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class AgentAgreement extends Model
 {
-    use HasFactory;
+    /**
+     * @return mixed
+     */
     public function user()
     {
         return $this->belongsToMany(User::class);
