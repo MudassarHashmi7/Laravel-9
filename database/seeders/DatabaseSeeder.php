b192d8cbd3adb39f67a6bea2ebd34511cfb7f247
Usman Jutt <usmanjutt.kodestudio@gmail.com>
2023-02-06 18:37:45 +0500

    Migration & Seeder Code clean

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\User;
-use App\Models\RoleUser;
-//use App\Models\RoleUser;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -23,21 +19,6 @@ class DatabaseSeeder extends Seeder
             PermissionSeeder::class,
             RoleSeeder::class,
             UserSeeder::class,
-           // RoleUserSeeder::class,
         ]);
-//        {
-//        DB::table('users')->insert([
-//            'name' => 'admin',
-//            'email' => 'admin@gmail.com',
-//            'password' => Hash::make('admin1234'),
-//        ]);
-//    }
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
     }
 }
